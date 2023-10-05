fc6d13a72218eeb3719b354f7a3e527934fb9dc2
Matt <matthieu.mastio@gmail.com>
2023-10-05 15:07:04 +0200

    Clone from the odyssee repository

---
+++ code/main.py
@@ -0,0 +1,102 @@
+#from IPython import embed; embed()
+from IPython import embed
+import os
+os.environ['USE_PYGEOS'] = '0'
+import sys
+import yaml
+import matplotlib
+from matplotlib import pyplot as plt
+import geo
+import network
+
+"""
+#parameters to set all_oneway to true but it's buggy
+utn = ox.settings.useful_tags_node
+oxna = ox.settings.osm_xml_node_attrs
+oxnt = ox.settings.osm_xml_node_tags
+utw = ox.settings.useful_tags_way
+oxwa = ox.settings.osm_xml_way_attrs
+oxwt = ox.settings.osm_xml_way_tags
+utn = list(set(utn + oxna + oxnt))
+utw = list(set(utw + oxwa + oxwt))
+ox.settings.all_oneway = True
+ox.settings.useful_tags_node = utn
+ox.settings.useful_tags_way = utw
+"""
+
+
+def get_matrices(network, POIs):
+    #
+    return
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"usage: python3 {sys.argv[0]} configuration_file")
+        exit(1)
+    yml_path = sys.argv[1]
+    with open(yml_path, "r") as yml_file:
+        cfg = yaml.safe_load(yml_file)
+    data_path = cfg["data_path"]
+    municipalities_file = cfg["municipalities_file"]
+    gtfs_path = cfg["gtfs_path"]
+    grid_size = cfg["grid_size"]
+
+    #Create the processed data directory
+    processed_path = f"{data_path}/processed/{municipalities_file.split('.')[0]}"
+
+    if not os.path.isdir(processed_path):
+        os.mkdir(processed_path)
+
+    gdf = geo.get_gdf(data_path, processed_path, municipalities_file)
+
+    network_b = network.Network(gdf, "bike", processed_path, gtfs_path)
+    network_w = network.Network(gdf, "walk", processed_path, gtfs_path)
+    network_d = network.Network(gdf, "drive", processed_path)
+    network_t = network.Network(gdf, "transit", processed_path, gtfs_path)
+
+    r1 = geo.random_point_in_area(gdf)
+    r2 = geo.random_point_in_area(gdf)
+    print("bike: ", network_b.shortest_path(r1, r2))
+    print("walk: ", network_w.shortest_path(r1, r2))
+    print("drive: ", network_d.shortest_path(r1, r2))
+    print("transit: ", network_t.shortest_path(r1, r2))
+
+    embed()
+
+
+    """
+    #find distance matrix between all restaurants to all restaurant
+    bbox = tuple(gdf.dissolve().to_crs(4326).bounds.iloc[0])
+    restaurants = osm.node_query(bbox[1], bbox[0], bbox[3], bbox[2],
+                                 tags='"amenity"="restaurant"')
+    restaurant_nodes = network.get_node_ids(restaurants.lon, restaurants.lat)
+    origs = [o for o in restaurant_nodes.values for d in restaurant_nodes.values]
+    dests = [d for o in restaurant_nodes.values for d in restaurant_nodes.values]
+    # this vectorized version of the shortest path computation is way more efficient
+    distances = network.shortest_path_lengths(origs, dests)
+    print(distances)
+
+    #find the closest restaurants to each node
+    network.set_pois(category = 'restaurants',
+                     maxdist = 1000,
+                     maxitems = 3,
+                     x_col = restaurants.lon,
+                     y_col = restaurants.lat)
+    results = network.nearest_pois(distance = 1000,
+                                   category = 'restaurants',
+                                   num_pois = 3,
+                                   include_poi_ids = True)
+    print(results.head())
+
+    #how many restaurants are within 20min meters of each node?
+    network.set(restaurant_nodes, name = 'restaurants')
+    accessibility = network.aggregate(distance = 20, type = 'count', name = 'restaurants')
+    accessibility.describe()
+    fig, ax = plt.subplots(figsize=(10,8))
+    plt.title('Restaurants within 20min transportation')
+    plt.scatter(network.nodes_df.x, network.nodes_df.y,
+                c=accessibility, s=1, cmap='YlOrRd',
+                norm=matplotlib.colors.LogNorm())
+    cb = plt.colorbar()
+    plt.show()
+    """
---
+++ code/network.py
@@ -0,0 +1,110 @@
+import osmnx as ox
+import pandas as pd
+import numpy as np
+import os
+from graph_utils import create_nx_graph, create_pdn_graph, get_integrated_graph
+import networkx as nx
+from IPython import embed
+
+class Network():
+
+    def __init__(self, gdf, mode, processed_path, gtfs_path=None):
+        if mode == "transit" and gtfs_path == None:
+            raise Exception("No gtfs provided when mode is set to transit")
+        self.processed_path = processed_path
+        self.gdf = gdf
+        self.mode = mode
+        self.gtfs_path = gtfs_path
+        self.ml, self.pdn = self.create_network()
+
+
+    def create_network(self):
+        area = self.gdf.dissolve().to_crs(4326)
+        path = f"{self.processed_path}/graph_{self.mode}.graphml"
+        if os.path.exists(path):
+            print(f"Loading {self.mode} network")
+            graph = ox.load_graphml(path)
+        else:
+            print(f"Downloading {self.mode} network")
+            #cf = '["highway"~"motorway|trunk|primary|secondary"]'
+            if self.mode == "transit":
+                if os.path.exists(f"{self.processed_path}/graph_walk.graphml"):
+                    graph = ox.load_graphml(path)
+                else:
+                    graph = ox.graph_from_polygon(area.geometry[0], network_type="walk")
+                urbanaccess_net = get_integrated_graph(self.gdf, graph, self.processed_path, self.gtfs_path)
+                graph = create_nx_graph(urbanaccess_net.net_nodes, urbanaccess_net.net_edges)
+                ox.save_graphml(graph, filepath=path)
+            else:
+                graph = ox.graph_from_polygon(area.geometry[0], network_type=self.mode)
+                if self.mode=="drive":
+                    graph = ox.add_edge_speeds(graph)
+                    graph = ox.add_edge_travel_times(graph)
+                elif self.mode=="bike":
+                    edges = ox.graph_to_gdfs(graph, nodes=False)
+                    edges = edges.to_crs("epsg:32633") #because bike network is projected
+                    travel_time = edges["length"] / (20/3.6) #TODO adapt bike speed to topography
+                    edges["travel_time"] = travel_time.values
+                    nx.set_edge_attributes(graph, values=edges["travel_time"], name="travel_time")
+                    nx.set_edge_attributes(graph, values=edges["geometry"], name="geometry")
+                elif self.mode=="walk":
+                    edges = ox.graph_to_gdfs(graph, nodes=False)
+                    travel_time =  edges["length"] / (4.8/3.6) #4.8 Km/H = 3 M/H
+                    edges["travel_time"] = travel_time.values
+                    nx.set_edge_attributes(graph, values=edges["travel_time"], name="travel_time")
+                ox.save_graphml(graph, filepath=path)
+        nodes, edges = ox.graph_to_gdfs(graph)
+        edges["weight"] = edges.travel_time
+        edges["from_int"]=edges.index.get_level_values(0)
+        edges["to_int"]=edges.index.get_level_values(1)
+        pdn_graph = create_pdn_graph(nodes, edges)
+        return graph, pdn_graph
+
+
+    def shortest_path(self, r1, r2):
+        req = pd.DataFrame([r1, r2], columns=["lon", "lat"])
+        nodes_ids = self.pdn.get_node_ids(req.lon, req.lat).values
+        shortest_path = self.pdn.shortest_path(nodes_ids[0], nodes_ids[1])
+        travel_time, distance = self.get_route_details(list(shortest_path))
+        res = { "shortest_path": shortest_path,
+                "travel_time"  : travel_time,
+                "distance"     : distance}
+        return res
+
+
+    def get_route_details(self, route):
+        """
+        Calculate travel time and distance for a given route on a graph.
+
+        Parameters:
+        - graph (NetworkX Graph): The graph representing the transportation network.
+        - route (list): A list of nodes representing the route to be analyzed.
+
+        Returns:
+        - travel_time (float): The total travel time along the route (sum of travel times for individual edges).
+        - distance (float): The total distance covered along the route (sum of edge lengths).
+        """
+        travel_time = 0
+        distance = 0
+        if not route:
+            return None, None
+        for i in range(len(route)-1):
+            data = self.ml.get_edge_data(route[i], route[i+1])[0]
+            travel_time += data["travel_time"]
+            if not np.isnan(data["length"]): #necessary because tt travels have nan distances
+                distance += data["length"]
+        return travel_time, distance
+
+
+    """
+    def get_matrices(graph, grid):
+        tt_matrix = []
+        d_matrix = []
+        for i, j in product(grid.index, grid.index):
+            route = ox.shortest_path(graph, grid.id.iloc[i], grid.id.iloc[j],
+                                 weight='travel_time', cpus=None)
+            tt, d = get_route_details(graph, route)
+            tt_matrix.append({"from_id": grid.id[i], "to_id": grid.id[j], "travel_time": tt})
+            d_matrix.append({"from_id": grid.id[i], "to_id": grid.id[j], "distance": d})
+        return pd.DataFrame(tt_matrix), pd.DataFrame(d_matrix)
+    """
